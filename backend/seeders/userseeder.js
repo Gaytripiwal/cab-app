3143dff416704958669f34bae16ce9598fb497ec
Gaytripiwal <piwalgaytri1@gmail.com>
2025-08-25 16:50:41 +0530

    initial files

--- backend/seeders/userseeder.js
+++ backend/seeders/userseeder.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
-const User = require('./models/User');
+const User = require('../models/User');
 require('dotenv').config();
 
 const seed = async () => {
@@ -16,20 +16,15 @@ const seed = async () => {
     // Insert user
     await User.create({
         role: 'company',
-        companyName: 'Om Cab Services',
-        companyId: 'OM001',
-        email: 'company@omcab.com',
-        password: companyPassword
+        employeeId: '001',
+        companyName: 'Arkarz',
+        companyId: 'AR001',
+        email: 'Arkarz@gmail.com',
+        password: await bcrypt.hash('123', 10) // <-- HASHED!
+
     });
 
-    // Insert user
-    await User.create({
-        role: 'user',
-        companyName: 'Om Cab Services',
-        companyId: 'OM001',
-        email: 'user@omcab.com',
-        password: userPassword
-    });
+
 
     console.log('Seeded users!');
     process.exit();
